eeccab39ea9c20747eeeb6509593c0df8b10a0f4
yyfbe <2861112448@qq.com>
2019-10-02 23:28:34 +0800

    Site updated: 2019-10-02 23:28:34

--- dist/music.js
+++ dist/music.js
@@ -1,17 +1,10 @@
 const ap = new APlayer({
     container: document.getElementById('aplayer'),
-    autoplay: false,
-    loop: 'all',
-    volume: 0.7,
-    listFolded: true,
-    listMaxHeight: 60,
+    fixed: true,
     audio:[{
         name: '初めてのデート',
         artist: 'SimorE',
-        url: 'http://music.163.com/song/media/outer/url?id=476592630.mp3',     
+        url: 'http://music.163.com/song/media/outer/url?id=35451104.mp3',
+        cover: '/source/images/cover.jpg',
     }]
 });
-<link rel="stylesheet" href="/dist/APlayer.min.css">
-<div id="aplayer"></div>
-<script type="text/javascript" src="/dist/APlayer.min.js"></script>
-<script type="text/javascript" src="/dist/music.js"></script>
